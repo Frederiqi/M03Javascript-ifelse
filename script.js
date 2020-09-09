b54ca7d8c9a7ca615609e9b7c36921a74338c14c
Frederiqi <68908971+Frederiqi@users.noreply.github.com>
2020-09-09 18:14:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const age = 30;
+if (age >= 18) {
+    console.log("Deze persoon mag naar binnen.")
+} else {
+    console.log("Deze persoon is te jong en wordt vriendelijk verzocht buiten te blijven.")
+}
+const isFemale = false;
+if (isFemale) {
+    console.log("Deze vrouw heeft toegang tot de ladiesnight.")
+} else {
+    console.log("Deze persoon is geen vrouw en kan niet naar de ladiesnight.")
+}
+const driverStatus = "Jan";
+if (driverStatus == "Bob") {
+    console.log("Deze bestuurder is de Bob en mag rijden.")
+} else {
+    console.log("Deze persoon mag helaas niet rijden.")
+}
